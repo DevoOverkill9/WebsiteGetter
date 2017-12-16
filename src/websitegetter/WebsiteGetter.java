cb911671bb0ff1358fbd388d0e9502a63dfa855c
Dev <devoohk2016@gmail.com>
2017-12-16 12:47:23 +0200

    Add files via upload

---
+++ src/websitegetter/WebsiteGetter.java
@@ -0,0 +1,12 @@
+package websitegetter;
+
+public class WebsiteGetter {
+
+   
+    public static void main(String[] args) {
+        mainForm mainform = new mainForm();
+        mainform.setLocationRelativeTo(null);
+        mainform.setVisible(true);
+    }
+    
+}
